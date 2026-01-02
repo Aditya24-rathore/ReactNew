3a228b5a602369252cd0f7b1ff74b6dec94e0594
Aditya24-rathore <ra4402538@gmail.com>
2026-01-02 01:03:53 -0800

    update

---
+++ Rn/src/Tailwind.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import "./Tailwind.css"
+const Tailwind = () => {
+  return (
+    <div>
+        <div className='w-100 h-100 font-bold  flex justify-center items-center border rounded-2xl'>
+        <h1 className='text-3xl'>React</h1>
+        <h1 className="bg-amber-200 text-black ">Tailwind css</h1>
+        </div>
+    </div>
+  )
+}
+
+export default Tailwind
